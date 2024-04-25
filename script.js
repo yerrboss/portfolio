b6c3bad0aaa9bfbf61d42b037a4e86a2ca254381
yerrboss <162325649+yerrboss@users.noreply.github.com>
2024-04-25 18:07:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: true,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+});
+
+
+
